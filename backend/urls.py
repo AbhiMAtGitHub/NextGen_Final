87e1d231d960d2454c6ce3ca15b5a582ef9fb55c
sujith_marpuri <sujith_marpuri@thbs.com>
2024-02-16 17:06:45 +0530

    New app added. Responsive charts are working in the sampleCharts app

--- backend/urls.py
+++ backend/urls.py
@@ -20,4 +20,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('prediction.urls')),
+    path('predict1/', include('sampleCharts.urls')),
 ]
---
+++ sampleCharts/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class SamplechartsConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'sampleCharts'
